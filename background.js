fa2daabddc14b709e2d97a2166a060096b7d7514
conoson <kos982000@gmail.com>
2021-11-16 23:06:24 +0900

    commit

--- background.js
+++ background.js
@@ -1,3 +1,5 @@
+let checker = false;
+
 class repeatBody{
     start = 0;
     end = 1;
@@ -7,22 +9,9 @@ class repeatBody{
     }
 }
 
-
-chrome.tabs.onCreated.addListener( function(tab) {
-    if(changeInfo.url){
-        chrome.scripting.executeScript({
-            target: {tabId: tab.id},
-            files: ['repeat.js']
-        });
-        chrome.scripting.insertCSS({
-            target: {tabId: tab.id},
-            files: ['style.css']
-        });
-    }
-});
-
 chrome.tabs.onUpdated.addListener( function(tabId, changeInfo) {
-    if(changeInfo.url){
+    if(changeInfo.url && !checker){
+        checker=true;
         chrome.scripting.executeScript({
             target: {tabId: tabId},
             files: ['repeat.js']
--- repeat.js
+++ repeat.js
@@ -1,106 +1,106 @@
-;let repeater = (function(){
-    class repeatInfo {
-        start = false;
-        end = false;
-        repeat = true;
-    }
-    function htmlToElement(html) {
-        var template = document.createElement('template');
-        html = html.trim(); // Never return a text node of whitespace as the result
-        template.innerHTML = html;
-        return template.content.firstChild;
-    }
-
-    let slider = `
-    <div class="middle"> <div class="multi-range-slider"> <!-- 진짜 슬라이더 --> <input type="range" id="input-left" /> <input type="range" id="input-right" /> <!-- 커스텀 슬라이더 --> <div class="slider"> <div class="track"></div> <div class="range"></div> <div class="thumb left"></div> <div class="thumb right"></div> </div> </div> </div>
-    `;
-    let sliderElement = htmlToElement(slider);
-    const inputLeft = document.getElementById("input-left");
-    const inputRight = document.getElementById("input-right");
-    const thumbLeft = document.querySelector(".slider > .thumb.left");
-    const thumbRight = document.querySelector(".slider > .thumb.right");
-    const range = document.querySelector(".slider > .range");
-    const video = document.querySelector('.html5-main-video');
-    
-    const setLeftValue = () => {
-        const _this = inputLeft;
-        const [min, max] = [parseInt(_this.min), parseInt(_this.max)];
-        // 교차되지 않게, 1을 빼준 건 완전히 겹치기보다는 어느 정도 간격을 남겨두기 위해. 
-        _this.value = Math.min(parseInt(_this.value), parseInt(inputRight.value) - 1);
-        // input, thumb 같이 움직이도록 
-        const percent = ((_this.value - min) / (max - min)) * 100;
-        thumbLeft.style.left = percent + "%";
-        range.style.left = percent + "%";
-    };
-    const setRightValue = () => {
-        const _this = inputRight;
-        const [min, max] = [parseInt(_this.min), parseInt(_this.max)];
-        // 교차되지 않게, 1을 더해준 건 완전히 겹치기보다는 어느 정도 간격을 남겨두기 위해. 
-        _this.value = Math.max(parseInt(_this.value), parseInt(inputLeft.value) + 1);
-        // input, thumb 같이 움직이도록 
-        const percent = ((_this.value - min) / (max - min)) * 100;
-        thumbRight.style.right = 100 - percent + "%";
-        range.style.right = 100 - percent + "%";
-    };
-
-    let init = (res)=>{
-        let data = res["data"];
-        data ||= {};
-        let code = location.search.match(/\?v=.+(&|$)/g)[0].replace(/[v?&\=]/g, '');
-        if(code){
-            document.body.append(sliderElement);
+if(repeater === undefined){
+    var repeater = (function(){
+        class repeatInfo {
+            start = false;
+            end = false;
+            repeat = true;
         }
-        let cur = data[code] || new repeatInfo();
+        function htmlToElement(html) {
+            var template = document.createElement('template');
+            html = html.trim(); // Never return a text node of whitespace as the result
+            template.innerHTML = html;
+            return template.content.firstChild;
+        }
+
+        let slider = `
+        <div class="middle"> <div class="multi-range-slider"> <!-- 진짜 슬라이더 --> <input type="range" id="input-left" /> <input type="range" id="input-right" /> <!-- 커스텀 슬라이더 --> <div class="slider"> <div class="track"></div> <div class="range"></div> <div class="thumb left"></div> <div class="thumb right"></div> </div> </div> </div>
+        `;
+        let sliderElement = htmlToElement(slider);
+        document.body.append(sliderElement);
+        const inputLeft = document.getElementById("input-left");
+        const inputRight = document.getElementById("input-right");
+        const thumbLeft = document.querySelector(".slider > .thumb.left");
+        const thumbRight = document.querySelector(".slider > .thumb.right");
+        const range = document.querySelector(".slider > .range");
+        const video = document.querySelector('.html5-main-video');
         
-        let infoElement = document.querySelector('ytd-video-primary-info-renderer');
-
-        sliderElement.style.width = infoElement.getBoundingClientRect().width + "px";
-        sliderElement.style.left = infoElement.getBoundingClientRect().x + "px";
-        sliderElement.style.top = infoElement.getBoundingClientRect().y + "px";
-
-        [inputLeft.min, inputLeft.max] = [0, video.duration];
-        [inputRight.min, inputRight.max] = [0, video.duration];
-        [inputLeft.value, inputRight.value] = [cur.start || 0, cur.end || video.duration];
-
-        function update() {
-            let info = new repeatInfo();
-            info.start = inputLeft.value;
-            info.end = inputRight.value;
-            data[code] = info;
-
-            chrome.storage.sync.set({ "data": data }, () => {
-                setTimeout(update, 5000);
-            })
+        const setLeftValue = () => {
+            const _this = inputLeft;
+            const [min, max] = [parseInt(_this.min), parseInt(_this.max)];
+            // 교차되지 않게, 1을 빼준 건 완전히 겹치기보다는 어느 정도 간격을 남겨두기 위해. 
+            _this.value = Math.min(parseInt(_this.value), parseInt(inputRight.value) - 1);
+            // input, thumb 같이 움직이도록 
+            const percent = ((_this.value - min) / (max - min)) * 100;
+            thumbLeft.style.left = percent + "%";
+            range.style.left = percent + "%";
         };
-        update();
-        setLeftValue();
-        setRightValue();
+        const setRightValue = () => {
+            const _this = inputRight;
+            const [min, max] = [parseInt(_this.min), parseInt(_this.max)];
+            // 교차되지 않게, 1을 더해준 건 완전히 겹치기보다는 어느 정도 간격을 남겨두기 위해. 
+            _this.value = Math.max(parseInt(_this.value), parseInt(inputLeft.value) + 1);
+            // input, thumb 같이 움직이도록 
+            const percent = ((_this.value - min) / (max - min)) * 100;
+            thumbRight.style.right = 100 - percent + "%";
+            range.style.right = 100 - percent + "%";
+        };
 
-        inputLeft.addEventListener("input", setLeftValue);
-        inputRight.addEventListener("input", setRightValue);
+        let init = (res)=>{
+            let data = res["data"];
+            data ||= {};
+            let code = location.search.match(/\?v=.+(&|$)/g)[0].replace(/[v?&\=]/g, '');
+            let cur = data[code] || new repeatInfo();
+            
+            let infoElement = document.querySelector('ytd-video-primary-info-renderer');
 
-        window.addEventListener('resize', function(e) {
-            setTimeout(() => {
-                let infoElement = document.querySelector('ytd-video-primary-info-renderer');
-                sliderElement.style.width = infoElement.getBoundingClientRect().width + "px";
-                sliderElement.style.left = infoElement.getBoundingClientRect().x + "px";
-                sliderElement.style.top = infoElement.getBoundingClientRect().y + "px";
-            }, 500)
-        }, false);
+            sliderElement.style.width = infoElement.getBoundingClientRect().width + "px";
+            sliderElement.style.left = infoElement.getBoundingClientRect().x + "px";
+            sliderElement.style.top = infoElement.getBoundingClientRect().y + "px";
 
-        video.addEventListener('timeupdate', function(e) {
-            var currentPos = video.currentTime;
+            [inputLeft.min, inputLeft.max] = [0, video.duration];
+            [inputRight.min, inputRight.max] = [0, video.duration];
+            [inputLeft.value, inputRight.value] = [cur.start || 0, cur.end || video.duration];
 
-            if (currentPos >= inputRight.value) {
-                video.currentTime = inputLeft.value
-            }
-        })
-    }
+            function update() {
+                let info = new repeatInfo();
+                info.start = inputLeft.value;
+                info.end = inputRight.value;
+                data[code] = info;
 
-    return {
-        init : init
-    }
-})();
+                chrome.storage.sync.set({ "data": data }, () => {
+                    setTimeout(update, 5000);
+                })
+            };
+            update();
+            setLeftValue();
+            setRightValue();
+
+            inputLeft.addEventListener("input", setLeftValue);
+            inputRight.addEventListener("input", setRightValue);
+
+            window.addEventListener('resize', function(e) {
+                setTimeout(() => {
+                    let infoElement = document.querySelector('ytd-video-primary-info-renderer');
+                    sliderElement.style.width = infoElement.getBoundingClientRect().width + "px";
+                    sliderElement.style.left = infoElement.getBoundingClientRect().x + "px";
+                    sliderElement.style.top = infoElement.getBoundingClientRect().y + "px";
+                }, 500)
+            }, false);
+
+            video.addEventListener('timeupdate', function(e) {
+                var currentPos = video.currentTime;
+
+                if (currentPos >= inputRight.value) {
+                    video.currentTime = inputLeft.value
+                }
+            })
+        }
+
+        return {
+            init : init
+        }
+    })();
+}
 
 
 function ready(fn) {
@@ -115,6 +115,7 @@ function ready(fn) {
 
 chrome.storage.sync.get(["data"], (res) => {
     ready(() => {
+        console.log(repeater);
         repeater.init(res);
     })
 })
